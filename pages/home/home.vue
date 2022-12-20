32f5d6b84c28eee192c3a15223c956a579ef25ee
LJApple <2439061874@qq.com>
2022-12-20 19:08:10 +0800

    banner

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -73,14 +73,11 @@
 		data() {
 			return {
 				bannerList: [{
-					image: 'https://cdn.uviewui.com/uview/swiper/swiper2.png',
-					title: '昨夜星辰昨夜风，画楼西畔桂堂东',
+					image: '/static/banner01.jpg'
 				}, {
-					image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
-					title: '身无彩凤双飞翼，心有灵犀一点通'
+					image: '/static/banner02.jpg'
 				}, {
-					image: 'https://cdn.uviewui.com/uview/swiper/swiper3.png',
-					title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					image: '/static/banner03.jpg'
 				}],
 				// 消息提醒
 				messageTip: {
